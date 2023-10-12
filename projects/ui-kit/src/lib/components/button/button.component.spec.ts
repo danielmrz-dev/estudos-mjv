efa803c24de84b280a4a0f1e255b104c53eb35df
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-10-12 13:15:30 +0200

    lesson: Overview of the Component Fixture

--- projects/ui-kit/src/lib/components/button/button.component.spec.ts
+++ projects/ui-kit/src/lib/components/button/button.component.spec.ts
@@ -1,11 +1,12 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ButtonComponent } from "./button.component";
 import { ButtonModule } from "./button.module";
 
 describe('ButtonComponent', () => {
+  let fixture: ComponentFixture<ButtonComponent>;
   beforeEach(() => {
     TestBed.configureTestingModule({imports: [ButtonModule]})
-    TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(ButtonComponent);
   });
   it('should test something...', () => {
     expect(true).toBe(true);
