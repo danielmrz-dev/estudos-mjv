173c12b896629618de41857ff1bfa7a5c17444f4
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-10-15 12:35:37 +0200

    lesson: exploring testbed features

--- projects/ui-kit/src/lib/components/button/button.component.spec.ts
+++ projects/ui-kit/src/lib/components/button/button.component.spec.ts
@@ -1,12 +1,16 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ButtonComponent } from "./button.component";
 import { ButtonModule } from "./button.module";
+import { DebugElement } from "@angular/core";
 
 describe('ButtonComponent', () => {
   let fixture: ComponentFixture<ButtonComponent>;
+  let el: DebugElement;
   beforeEach(() => {
     TestBed.configureTestingModule({imports: [ButtonModule]})
     fixture = TestBed.createComponent(ButtonComponent);
+    el = fixture.debugElement;
+    fixture.detectChanges(); // initial CD. triggers ngOnInit
   });
   it('should test something...', () => {
     expect(true).toBe(true);
