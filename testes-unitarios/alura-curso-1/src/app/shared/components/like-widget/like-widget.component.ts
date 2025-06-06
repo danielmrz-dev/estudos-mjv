efb39b01ab4c7eeabf2276c949a568e1d0a8bcbe
Daniel Mariz <danielmrz.dev@gmail.com>
2025-06-06 16:55:05 -0300

    rimeiros testes na UniqueIdService

---
+++ testes-unitarios/alura-curso-1/src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-like-widget',
+  imports: [FontAwesomeModule],
+  templateUrl: './like-widget.component.html',
+  styleUrl: './like-widget.component.scss'
+})
+export class LikeWidgetComponent {
+
+}
