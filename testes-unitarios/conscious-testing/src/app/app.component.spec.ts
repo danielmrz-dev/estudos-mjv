a5365fc3a13938f1f6c03ff50c682e86a43b0756
Daniel Mariz <danielmrz.dev@gmail.com>
2025-06-11 18:01:46 -0300

    Diferenças entre o done() e o jasmine.clock()

--- testes-unitarios/conscious-testing/src/app/app.component.spec.ts
+++ testes-unitarios/conscious-testing/src/app/app.component.spec.ts
@@ -1,29 +1,49 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+function pollingTask(task: any, interval = 1000) {
+  return setInterval(() => task(), interval);
+}
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    jasmine.clock().install();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  afterEach(() => {
+    jasmine.clock().uninstall()
   });
 
-  it(`should have the 'conscious-testing' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('conscious-testing');
+  // it('Testing the use of Done Function', (done: DoneFn) => {
+  //   setTimeout(() => {
+  //     expect(1).toBe(1);
+  //     done();
+  //   }, 2000);
+  // });
+  
+  it('Testing the use of jasmine.clock function', () => {
+    setTimeout(() => {
+      expect(1).toBe(1);
+    }, 2000);
+    jasmine.clock().tick(2000);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, conscious-testing');
+  it('should execute async task with an interval', () => {
+    const pollingTaskSpy = jasmine.createSpy('pollingTask');
+    pollingTask(pollingTaskSpy, 2000);
+    expect(pollingTaskSpy).not.toHaveBeenCalled();
+    jasmine.clock().tick(1000);
+    expect(pollingTaskSpy).not.toHaveBeenCalled();
+    jasmine.clock().tick(1000);
+    expect(pollingTaskSpy).toHaveBeenCalledTimes(1);
+    jasmine.clock().tick(1000);
+    expect(pollingTaskSpy).toHaveBeenCalledTimes(1);
+    jasmine.clock().tick(1000);
+    expect(pollingTaskSpy).toHaveBeenCalledTimes(2);
   });
+  
+
 });
