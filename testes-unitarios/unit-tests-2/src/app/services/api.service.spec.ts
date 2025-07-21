67342bd43c0c9127ce58742594fa61839800ef29
Daniel Mariz <danielmrz.dev@gmail.com>
2025-07-21 11:54:30 -0300

    Teste

--- testes-unitarios/unit-tests-2/src/app/services/api.service.spec.ts
+++ testes-unitarios/unit-tests-2/src/app/services/api.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
